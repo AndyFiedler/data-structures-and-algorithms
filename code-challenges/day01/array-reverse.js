e7920c9816b673690ee15204a12805f45b42f5db
Andy Fiedler <andyfidd@gmail.com>
2019-09-17 10:13:48 -0500

    andy added some basic array reverse stuff for cc1

---
+++ code-challenges/day01/array-reverse.js
@@ -0,0 +1,21 @@
+
+// let arr = 
+// function reverse(array) {
+//     var output = [];
+//     while (array.length) {
+//       output.push(array.pop());
+//     }
+  
+//     return output;
+//   }
+let array = [1, 2, 3, 4, 5];
+  function reverse(array){
+    let result = [];
+    for (let i = 0; i<= array.length; i++){
+        result.push(array.pop());
+    }
+
+    return result;
+}
+
+console.log(result);
