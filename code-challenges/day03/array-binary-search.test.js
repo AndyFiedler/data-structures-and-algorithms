a02158be714c75859a8861be4650e0edf2b704fa
Andy Fiedler <andyfidd@gmail.com>
2019-09-19 08:54:32 -0500

    andy replaced for loop with while

---
+++ code-challenges/day03/array-binary-search.test.js
@@ -0,0 +1,16 @@
+let array1 = [4, 8, 15, 16, 23, 42];
+let array2 = [11, 22, 33, 44, 55, 66, 77];
+
+const searchBinarily =(array, target) => {
+    var n = 0;
+
+while (n !== target) {
+  n++;
+}
+
+console.log(n);
+}
+
+// for(let i =0; i < array.length; i++){
+        
+// }
