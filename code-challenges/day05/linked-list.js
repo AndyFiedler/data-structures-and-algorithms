270667c4aeef42a44b43a0f438208b531efc9af8
Andy Fiedler <andyfidd@gmail.com>
2019-09-20 16:04:56 -0500

    andy added two classes and starter tests

--- code-challenges/day05/linked-list.js
+++ code-challenges/day05/linked-list.js
@@ -1,10 +1,23 @@
 'use strict';
+
 let current = this.head;
 let previous;
 let count = 0;
 
+let results = [];
+
+if(index ===0) {
+    this.head = current.next;
+} else {
+    while(count < index) {
+        count++;
+        previous = current;
+        current = current.next;
+    }
+}
+
 class Node {
-    constructor(data, next = null){
+    constructor(data, next = null) {
         this.data = data;
         this.next = next;
     }
@@ -15,8 +28,31 @@ class LinkedList {
         this.head = null;
         this.size = 0;
     }
+
+    //insert first node
+    firstNode(data) {
+        this.head = new Node(data, this.head);
+    }
+
+    //insert last node 
+    lastNode(data) {
+        this.tail = new Node(data, this.tail);
+    }
+    
+    //get at index(includes)
+    readAtIndex(data) {
+        
+    }
+
 };
 
-insertFirst(data) {
-    this.head = new Node(data, this.head);
-}
+return results;
+module.exports = LinkedList;
+
+
+
+
+
+
+
+//return list(toString)
---
+++ code-challenges/day05/linked-list.test.js
@@ -0,0 +1,23 @@
+'use strict';
+const linkList= require('LinkedList');
+
+describe('can succesfully instantiate an empty linked list')
+it()
+
+describe('can properly insert into the linked list')
+it()
+
+describe('The head property will properly point to the first node in the linked list')
+it()
+
+describe('Can properly insert multiple nodes into the linked list')
+it()
+
+describe('will return true when finding a value in the linked list that exists')
+it()
+
+describe('will return false when searching for a value in the linked list that does not exist')
+it()
+
+describe('Can properly return a collection of all the values that exist in the linked list')
+it()
