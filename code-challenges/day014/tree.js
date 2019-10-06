75f9cb87f013baceb9412ae492edf17563b429b1
Andy Fiedler <andyfidd@gmail.com>
2019-10-06 14:49:22 -0500

    banjo added methods

--- code-challenges/day014/tree.js
+++ code-challenges/day014/tree.js
@@ -15,6 +15,36 @@ class BinarySearchTree {
     } 
 }
 
+inorder(node) 
+{ 
+    if(node !== null) 
+    { 
+        this.inorder(node.left); 
+        console.log(node.data); 
+        this.inorder(node.right); 
+    } 
+} 
+
+preorder(node) 
+{ 
+    if(node != null) 
+    { 
+        console.log(node.data); 
+        this.preorder(node.left); 
+        this.preorder(node.right); 
+    } 
+} 
+
+postorder(node) 
+{ 
+    if(node != null) 
+    { 
+        this.postorder(node.left); 
+        this.postorder(node.right); 
+        console.log(node.data); 
+    } 
+} 
+
 // Define a method for each of the depth first traversals called preOrder, inOrder, and postOrder which returns an array of the values, ordered appropriately.
 
 preOrder()
