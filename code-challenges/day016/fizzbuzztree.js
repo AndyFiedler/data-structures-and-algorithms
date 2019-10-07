e1356b7d78116bcebe59776c45a4cdf329b4ef3d
Andy Fiedler <andyfidd@gmail.com>
2019-10-07 18:14:41 -0500

    banjo added fizzbuzz and some tests

--- code-challenges/day016/fizzbuzztree.js
+++ code-challenges/day016/fizzbuzztree.js
@@ -1,15 +1,21 @@
 'use strict';
 
-const binarySearchTree = require('./day014/binarySearchTree');
-
 function fizzBuzzTree(tree){
-  if(node.data % 15 === 0){
-
+  function inOrder(node) {
+    if (node !== null) {
+      inOrder(node.left);
+      if(node.data % 15 === 0){
+        node.data = 'FizzBuzz';
+      } else if(node.data % 5 === 0){
+        node.data = 'Buzz';
+      } else if(node.data % 3 === 0){
+        node.data = 'Fizz';
+      }
+      inOrder(node.right);
+    }
   }
-  if(node.data % 5 === 0){
-
-  }
-  if(node.data % 3 === 0){
-
-  }
+  inOrder(tree.root);
+  return tree;
 }
+
+module.exports = fizzBuzzTree;
