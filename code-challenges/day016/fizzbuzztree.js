05bd0b35321a6a64bd73f6dc816e4e42b9417388
Andy Fiedler <andyfidd@gmail.com>
2019-10-07 13:38:20 -0500

    banjo added function scaffold

---
+++ code-challenges/day016/fizzbuzztree.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const binarySearchTree = require('./day014/binarySearchTree');
+
+function fizzBuzzTree(tree){
+  if(node.data % 15 === 0){
+
+  }
+  if(node.data % 5 === 0){
+
+  }
+  if(node.data % 3 === 0){
+
+  }
+}
