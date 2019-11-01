8763e0ec8609e17814360e70375d0635b9e57b30
Andy Fiedler <andyfidd@gmail.com>
2019-11-01 14:08:40 -0500

    Banjo added methods to graph.js

--- code-challenges/day035/graph.js
+++ code-challenges/day035/graph.js
@@ -3,8 +3,23 @@
 class Graph {
   constructor(numOfVertices){
     this.numOfVertices = numOfVertices;
-    this.AdjList = new Map();
+    this.AdjList = [];
   }
 
-  
+  addVertex(value) {
+    let vertex = new Node(value);
+    this.nodes.set(value, vertex);
+    return vertex;
+  }
+  addAdjacent(vertex){
+    this.AdjList.push(vertex);
+  }
+
+  addEdge(from, to) {
+    let fromVertex = this.addVertex(to);
+    let toVertex = this.addVertex(from);
+    fromVertex.addAdjacent(toVertex);
+    return [fromVertex, toVertex];
+  }
+
 }
